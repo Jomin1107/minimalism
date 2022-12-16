34c01dccca3fb6bbe16f77d6eb5028586bc08a1b
admin <admin@DESKTOP-ABDNPQ5>
2022-12-16 18:57:43 +0900

    .

--- src/main/java/com/oracle/minimalism/configuration/auth/PrincipalDetailsService.java
+++ src/main/java/com/oracle/minimalism/configuration/auth/PrincipalDetailsService.java
@@ -1,11 +1,9 @@
 package com.oracle.minimalism.configuration.auth;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import com.oracle.minimalism.dto.UserDto;
 import com.oracle.minimalism.mapper.UserMapper;
@@ -22,7 +20,7 @@ public class PrincipalDetailsService implements UserDetailsService {
     // 스프링이 로그인 요청을 가로챌 때 , username password 변수를 가로채는데
     // password 부분 처리는 알아서함
     // username이 DB에 있는지만 확인해서 리턴해주면 됨
-    // 이거 안만들어주면  username 이 그냥 user로 들어감 강제로 넣어줘야댐
+    // 이거 안만들어주면  username 이 그냥 user로 들어감 강제로 넣어줘야함
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
